01bd588995dff80288c80126bac5e4fc4e034c15
reneboygarcia <reneboygarcia@gmail.com>
2026-06-13 07:47:07 +0800

    build(homebrew): configure branch tracking and standard environment for HEAD builds

--- Formula/twitdl.rb
+++ Formula/twitdl.rb
@@ -1,10 +1,11 @@
 class Twitdl < Formula
+  env :std
   desc "Interactive CLI tool to download videos from Twitter/X"
   homepage "https://github.com/reneboygarcia/twitter_video"
   url "https://github.com/reneboygarcia/twitter_video/archive/refs/tags/v0.2.0.tar.gz"
   version "0.2.0"
   sha256 "PLACEHOLDER_SHA256"
-  head "file:///Users/reneboygarcia/Documents/Github%20Projects/Twitter%20Video%20Downloader", :using => :git, :branch => "feat/rust-061326"
+  head "file:///Users/reneboygarcia/Documents/Github%20Projects/Twitter%20Video%20Downloader", using: :git, branch: "feat/rust-061326"
 
   depends_on "rust" => :build
   depends_on "yt-dlp"
