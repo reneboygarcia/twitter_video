6a808205a52a718821b00f493b6ce77684345249
reneboygarcia <reneboygarcia@gmail.com>
2026-06-13 08:21:06 +0800

    chore: update Homebrew formula to v0.2.1

--- Formula/twitdl.rb
+++ Formula/twitdl.rb
@@ -2,9 +2,9 @@ class Twitdl < Formula
   env :std
   desc "Interactive CLI tool to download videos from Twitter/X"
   homepage "https://github.com/reneboygarcia/twitter_video"
-  url "https://github.com/reneboygarcia/twitter_video/archive/refs/tags/v0.2.0.tar.gz"
-  version "0.2.0"
-  sha256 "68cea248adf9dec4ad6b1e94eb7a2ebd6ff2269c7c29b131b8bd602423bcabe7"
+  url "https://github.com/reneboygarcia/twitter_video/archive/refs/tags/v0.2.1.tar.gz"
+  version "0.2.1"
+  sha256 "27016ee434faf1ff5af7f6beb5a03b0b3c19e8d1170d4d25e1d628abfcd41c0a"
   head "https://github.com/reneboygarcia/twitter_video.git", branch: "main"
 
   depends_on "rust" => :build
