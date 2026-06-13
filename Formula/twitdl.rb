c38e0605433b96591fbee69ea77fdd0c6293406b
reneboygarcia <reneboygarcia@gmail.com>
2026-06-13 21:13:12 +0800

    Update Formula/twitdl.rb for v0.2.7

--- Formula/twitdl.rb
+++ Formula/twitdl.rb
@@ -2,9 +2,9 @@ class Twitdl < Formula
   env :std
   desc "Interactive CLI tool to download videos from Twitter/X"
   homepage "https://github.com/reneboygarcia/twitter_video"
-  url "https://github.com/reneboygarcia/twitter_video/archive/refs/tags/v0.2.6.tar.gz"
-  version "0.2.6"
-  sha256 "9d2339753c46ae95593c5d7a337523e0df0c74d13576b96f1c57ee57cb62af47"
+  url "https://github.com/reneboygarcia/twitter_video/archive/refs/tags/v0.2.7.tar.gz"
+  version "0.2.7"
+  sha256 "02ab5e5ea24cb0d79078b61e52ecd1bab3ad94dd00a5c6ec2f71f0fbc92e6dde"
   head "https://github.com/reneboygarcia/twitter_video.git", branch: "main"
 
   depends_on "rust" => :build
